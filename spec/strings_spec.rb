4fb2789531337f1ff0e1f5134b7c5e121e5254ab
vitto2 <vitor2071@gmail.com>
2023-03-23 07:24:56 -0300

    mudando forma de criação de instância da classe

--- spec/strings_spec.rb
+++ spec/strings_spec.rb
@@ -2,13 +2,15 @@ require "./strings_tasks.rb"
 
 describe "string" do 
 
+    let(:substring) {TaskString.new}
+
     it "Checks for existence of substring" do 
-        substring = TaskString.new
         expect(substring.substring?("abcdef","abc")).to eq(true)
     end 
 
     it "checks for the absence of a substring in another string " do 
-        substring = TaskString.new
         expect(substring.substring?("abcdef","out")).to eq(false)
     end 
+
+    
 end 
