ac45a5abffb068952800374dc591594aad113d04
vitto2 <vitor2071@gmail.com>
2023-03-23 08:27:51 -0300

    subindo exercícios 3 e 4 com testes

--- spec/strings_spec.rb
+++ spec/strings_spec.rb
@@ -12,5 +12,13 @@ describe "string" do
         expect(substring.substring?("abcdef","out")).to eq(false)
     end 
 
+    it "Remove a substring from any string" do 
+        expect(substring.removeStr("abcdef","abc")).to eq("def")
+    end
+
+    it "Remove blank space and line break" do 
+        expect(substring.transformStr("texto com espaço")).to eq("textocomespaço")
+    end 
+
     
 end 
--- strings_tasks.rb
+++ strings_tasks.rb
@@ -4,4 +4,12 @@ class TaskString
         string.include?(substring) ? true : false
     end 
 
+    def removeStr(string,substring) 
+         string.empty? || substring.empty? && substring?(string,substring) ? false : string.delete(substring)     
+    end 
+
+    def transformStr(string) 
+        string.split(" ").join("")
+    end 
+
 end 
